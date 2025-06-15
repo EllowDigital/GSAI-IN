d6c9f2e65cccb1c02fa098c7222d0a7f71b16e5a
Sarwan Yadav <sarwanyadav6174@gmail.com>
2025-06-15 07:48:51 +0530

    Refactor Index page layout: reorganize sections, remove duplicate imports, and enhance readability

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,33 +3,58 @@ import HeroSection from "../components/HeroSection";
 import AboutSection from "../components/AboutSection";
 import FounderSection from "../components/FounderSection";
 import ProgramsSection from "../components/ProgramsSection";
+import EventsSection from "../components/EventsSection";
+import NewsSection from "../components/NewsSection";
 import BlogNewsSection from "../components/BlogNewsSection";
 import GallerySection from "../components/GallerySection";
 import RecognitionAffiliationsSection from "../components/RecognitionAffiliationsSection";
 import FaqSection from "../components/FaqSection";
 import LocationSection from "../components/LocationSection";
 import FooterSection from "../components/FooterSection";
-import NewsSection from "../components/NewsSection";
-import EventsSection from "../components/EventsSection";
 
 // Main page for Ghatak Sports Academy India™
 const Index = () => {
   return (
     <div className="bg-background w-full min-h-screen flex flex-col font-montserrat">
+      {/* Sticky Navbar */}
       <Navbar />
+
       <main className="pt-16 md:pt-20 flex-1 flex flex-col gap-0">
+        {/* Hero / Landing Banner */}
         <HeroSection />
-        <EventsSection />
+
+        {/* About Academy */}
         <AboutSection />
+
+        {/* Founder Story */}
         <FounderSection />
+
+        {/* Training Programs */}
         <ProgramsSection />
+
+        {/* Upcoming Events */}
+        <EventsSection />
+
+        {/* Latest News */}
         <NewsSection />
+
+        {/* Blog Articles or Updates */}
         <BlogNewsSection />
+
+        {/* Academy Photo Gallery */}
         <GallerySection />
-        <FaqSection />
-        <LocationSection />
+
+        {/* Recognitions and Affiliations */}
         <RecognitionAffiliationsSection />
+
+        {/* Frequently Asked Questions */}
+        <FaqSection />
+
+        {/* Academy Location */}
+        <LocationSection />
       </main>
+
+      {/* Footer */}
       <FooterSection />
     </div>
   );
