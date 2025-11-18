3f0378adeadd035af1d4b1a69ce5528b20f4da60
Sarwan Yadav <sarwanyadav6174@gmail.com>
2025-11-18 14:08:59 +0530

    delete all

--- src/data/navLinks.ts
+++
@@ -1,9 +0,0 @@
-export const navLinks = [
-  { name: 'Home', href: '#hero' },
-  { name: 'About', href: '#about' },
-  { name: 'Programs', href: '#programs' },
-  { name: 'Gallery', href: '#gallery' },
-  { name: 'Blog/News', href: '#blog' },
-  { name: 'FAQ', href: '#faq' },
-  { name: 'Location', href: '#location' },
-];
